a5614f71a1fb5770421713d98c6ebcbe3b34a0ce
Yun <bsko1024@gmail.com>
2023-05-31 19:35:10 +0900

    서버에서 신호 받고 이동시키기

---
+++ app/src/main/java/com/example/temisdk/Server.java
@@ -0,0 +1,46 @@
+package com.example.temisdk;
+import java.util.Map;
+import com.robotemi.sdk.Robot;
+
+public class Server {
+    private Robot robot;
+
+    public Server() {
+        robot = Robot.getInstance();
+    }
+
+    public void processApiResponse(Map<String, String> apiResponse) {
+        String location = apiResponse.get("location");
+
+        if (location != null) {
+            TemiController temiController = new TemiController();
+            temiController.moveToLocation(location);
+        } else {
+            // 기타 처리 로직
+            // 예시로서 'a', 'b', 'c' 이외의 경우에는 여기에 추가적인 로직을 구현할 수 있습니다.
+        }
+    }
+}
+
+class TemiController {
+    private Robot robot;
+
+    public TemiController() {
+        robot = Robot.getInstance();
+    }
+
+    public void moveToLocation(String location) {
+        if (location.equals("a")) {
+            // 'a'로 지정된 위치로 이동
+            robot.goTo("a");
+        } else if (location.equals("b")) {
+            // 'b'로 지정된 위치로 이동
+            robot.goTo("b");
+        } else if (location.equals("c")) {
+            // 'c'로 지정된 위치로 이동
+            robot.goTo("c");
+        } else {
+            // 움직이지 않는 처리 또는 기타 처리 로직
+        }
+    }
+}
