31e62d79c01c2ae77b9474d39249bd60ad380253
bbbbgggghh <trueju0407@gmail.com>
2023-05-31 18:28:42 +0900

    asdf

--- app/src/main/java/com/example/temisdk/MainActivity1.java
+++ app/src/main/java/com/example/temisdk/MainActivity1.java
@@ -1,5 +1,6 @@
 package com.example.temisdk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,19 +30,14 @@ public class MainActivity1 extends AppCompatActivity {
                 //서버에서 센서 신호 받아서 이동, 위치 알림 보내기, 암구호 요청(입력페이지 필요함 intent),
                 //암구호 입력시 홈베이스 이동, 미입력시 촬영 및 따라가기, 관리자 도착시 홈베이스
 
-
-
                 //서버에 야간경비 시작했다고 보냄 -> 인체감지 센서 활성화
 
                 //서버에서 센서신호 받음
                 robot.goTo("A"); //센서위치로 이동
                 //서버로 테미 위치 보내기
 
-                //암구호 입력창 인텐트
-                //if 암구호 입력 성공
-                robot.goTo("B"); //홈베이스로 이동
-                  //  else
-                robot.beWithMe(); //따라가기
+                Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                startActivity(intent);
 
                 //관리자 도착 시
                 //robot.goTo("B");
--- app/src/main/java/com/example/temisdk/MainActivity2.java
+++ app/src/main/java/com/example/temisdk/MainActivity2.java
@@ -1,9 +1,11 @@
 package com.example.temisdk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,103 +19,33 @@ import com.robotemi.sdk.listeners.OnBeWithMeStatusChangedListener;
 
 import org.jetbrains.annotations.NotNull;
 
-public class MainActivity2 extends AppCompatActivity implements
-        Robot.TtsListener,
-        Robot.ConversationViewAttachesListener,
-        OnBeWithMeStatusChangedListener {
-
-    TextView textState;
-    Button buttonSpeak;
-    Button buttonFollow;
-    Button buttonBack;
+public class MainActivity2 extends AppCompatActivity {
+    Button btn;
+    EditText et;
     Robot robot;
-    CustomTtsListener customTtsListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        textState = findViewById(R.id.textState);
-        buttonSpeak = findViewById(R.id.buttonSpeak);
-        buttonFollow = findViewById(R.id.buttonFollow);
-        buttonBack = findViewById(R.id.buttonBack);
+        btn = (Button) findViewById(R.id.btn);
+        et = (EditText) findViewById(R.id.edt);
         robot = Robot.getInstance();
-        customTtsListener = new CustomTtsListener();
 
-        buttonSpeak.setOnClickListener(new View.OnClickListener() {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                robot.speak(TtsRequest.create("Hello Temi!",true));
-            }
-        });
-
-        buttonFollow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (buttonFollow.getText().equals("따라가기")) {
+            public void onClick(View v){
+                if(et.getText().toString() == "나비"){
+                    robot.goTo("B");
+                    Intent intent = new Intent(MainActivity2.this, MainActivity1.class);
+                    startActivity(intent);
+                }
+                else{
                     robot.beWithMe();
-                    buttonFollow.setText("중지");
-                } else if (buttonFollow.getText().equals("중지")) {
-                    robot.stopMovement();
-                    buttonFollow.setText("따라가기");
+                    //관리자 호출
                 }
             }
         });
 
-        buttonBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Listener 객체추가, this 추가
-        robot.addTtsListener(this);
-        robot.addConversationViewAttachesListenerListener(this);
-        robot.addOnBeWithMeStatusChangedListener(this);
-        // Listener 재사용
-        robot.addTtsListener(customTtsListener);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        robot.removeTtsListener(this);
-        robot.removeConversationViewAttachesListenerListener(this);
-        robot.removeOnBeWithMeStatusChangedListener(this);
-        robot.removeTtsListener(customTtsListener);
-    }
-
-
-    @Override
-    public void onTtsStatusChanged(@NotNull TtsRequest ttsRequest) {
-        textState.setText("테미가 말하고 있습니다");
-        Log.i("Temi","Tts Status changed! "+ttsRequest.getStatus()+" - Original");
-    }
-
-    @Override
-    public void onConversationAttaches(boolean b) {
-        textState.setText("테미가 대화 중입니다");
-    }
-
-    @Override
-    public void onBeWithMeStatusChanged(@NotNull String s) {
-        switch (s) {
-            case OnBeWithMeStatusChangedListener.SEARCH:
-                textState.setText("테미가 당신을 찾는 중입니다");
-                break;
-
-            case OnBeWithMeStatusChangedListener.START:
-                textState.setText("테미가 당신을 찾았습니다");
-                break;
-
-            case OnBeWithMeStatusChangedListener.TRACK:
-                textState.setText("테미가 따라가는 중입니다");
-                break;
-        }
     }
 }
