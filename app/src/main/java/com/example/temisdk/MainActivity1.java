8a3a025101bd766ea6cbdd7a29c81b1a45b9bb10
bbbbgggghh <trueju0407@gmail.com>
2023-06-07 19:10:40 +0900

    firebase

--- app/src/main/java/com/example/temisdk/MainActivity1.java
+++ app/src/main/java/com/example/temisdk/MainActivity1.java
@@ -8,7 +8,14 @@ import android.util.Log;
 
 import okhttp3.*;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.robotemi.sdk.Robot;
 
 public class MainActivity1 extends AppCompatActivity {
@@ -16,6 +23,9 @@ public class MainActivity1 extends AppCompatActivity {
     private Button btn;
 
     private Robot robot;
+
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference = firebaseDatabase.getReference("sensor");
 
     NetworkTask networkTask;
 
@@ -30,20 +40,41 @@ public class MainActivity1 extends AppCompatActivity {
 
         networkTask = new NetworkTask();
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        databaseReference.child("sensor").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                //서버에서 센서 신호 받아서 이동, 위치 알림 보내기, 암구호 요청(입력페이지 필요함 intent),
-                //암구호 입력시 홈베이스 이동, 미입력시 촬영 및 따라가기
-
-                //서버에 야간경비 시작했다고 보냄 -> 인체감지 센서 활성화
-
-                //서버에서 센서신호 받음
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Object location = snapshot.getValue();
+                if(location == "A"){
                     robot.goTo("A"); //센서위치로 이동
                     networkTask.location = "A";
                     networkTask.execute();
                     Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
                     startActivity(intent);
+                }
+                if(location == "B"){
+                    robot.goTo("B"); //센서위치로 이동
+                    networkTask.location = "B";
+                    networkTask.execute();
+                    Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                    startActivity(intent);
+                }
+                if(location == "C"){
+                    robot.goTo("C"); //센서위치로 이동
+                    networkTask.location = "C";
+                    networkTask.execute();
+                    Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                    startActivity(intent);
+                }
+                if(location == "D"){
+                    robot.goTo("D"); //센서위치로 이동
+                    networkTask.location = "D";
+                    networkTask.execute();
+                    Intent intent = new Intent(MainActivity1.this, MainActivity2.class);
+                    startActivity(intent);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
             }
         });
     }
